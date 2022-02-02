904081d0d909554f4e9742211550eb5396321a98
Obed Marquez Parlapiano <obed.marquezp@gmail.com>
2022-02-02 18:47:53 +0100

    feat(admin): show notice if Unicorn isn't enabled

--- functions.php
+++ functions.php
@@ -1,1 +1,15 @@
 <?php
+
+add_action( 'admin_notices', 'warn_if_unicorn_is_disabled' );
+
+function warn_if_unicorn_is_disabled() {
+    if (defined('__UNICORN_DIR__')){
+        return;
+    }
+
+    ?>
+    <div class="notice notice-error is-dismissible">
+        <p>You're using Unicorn's Zero Theme but Unicorn is not enabled. This isn't supported.</p>
+    </div>
+    <?php
+}
