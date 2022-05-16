c31ebb0bbefb870fab6264001d21978c80c8c636
Obed Marquez Parlapiano <obed.marquezp@gmail.com>
2022-05-17 01:57:31 +0200

    refactor: rename everything to breakdance

--- functions.php
+++ functions.php
@@ -1,15 +1,15 @@
 <?php
 
-add_action( 'admin_notices', 'warn_if_unicorn_is_disabled' );
+add_action( 'admin_notices', 'warn_if_breakdance_is_disabled' );
 
-function warn_if_unicorn_is_disabled() {
-    if (defined('__UNICORN_DIR__')){
+function warn_if_breakdance_is_disabled() {
+    if (defined('__BREAKDANCE_DIR__')){
         return;
     }
 
     ?>
     <div class="notice notice-error is-dismissible">
-        <p>You're using Unicorn's Zero Theme but Unicorn is not enabled. This isn't supported.</p>
+        <p>You're using Breakdance's Zero Theme but Breakdance is not enabled. This isn't supported.</p>
     </div>
     <?php
 }
