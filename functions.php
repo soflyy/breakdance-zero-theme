77f3eef0dcc04545e1797c18273e3e1e3c5cd004
Obed Parlapiano <obed.marquezp@gmail.com>
2022-11-18 18:06:38 -0800

    feat: add featured image support (#10)

--- functions.php
+++ functions.php
@@ -4,6 +4,8 @@ if (!function_exists('breakdance_zero_theme_setup')) {
     function breakdance_zero_theme_setup()
     {
         add_theme_support('title-tag');
+        add_theme_support( 'post-thumbnails' );
+
     }
 }
 
